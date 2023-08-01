ee94f23c70c33cba7b16442778df962fdf7973a4
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-08-01 16:57:48 +0100

    create website from bucket object

--- index.ts
+++ index.ts
@@ -3,16 +3,37 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket", {
+    website: {
+        indexDocument: "index.html",
+    },
+});
+
+// Define file ownership settings on bucket
+const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
+    bucket: bucket.id,
+    rule: {
+        objectOwnership: "ObjectWriter"
+    }
+});
+
+// Allow bucket to be accessed publicly
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: false,
+});
 
 // Create an S3 Bucket object (static site)
 const bucketObject = new aws.s3.BucketObject("index.html", {
     bucket: bucket.id,
     source: new pulumi.asset.FileAsset("./index.html"),
     contentType: "text/html",
-});
+    acl: "public-read",
+}, { dependsOn: publicAccessBlock });
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
 // Export the bucket object ID
 export const bucketObjectId = bucketObject.id;
+// Export the bucket endpoint
+export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
