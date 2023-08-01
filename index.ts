87d2eb3c5ea70890401420127b22e4f3540822fa
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-08-01 16:50:06 +0100

    add bucket object index.html

--- index.ts
+++ index.ts
@@ -5,5 +5,14 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
 
+// Create an S3 Bucket object (static site)
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("./index.html"),
+    contentType: "text/html",
+});
+
 // Export the name of the bucket
 export const bucketName = bucket.id;
+// Export the bucket object ID
+export const bucketObjectId = bucketObject.id;
